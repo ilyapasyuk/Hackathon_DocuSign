1f39c97571a87574e255e14fac4be7764ead0974
ilyapasyuk <pasyuk.com@gmail.com>
2019-06-02 18:36:17 +0300

    add cards

---
+++ src/Components/Documents/index.jsx
@@ -0,0 +1,76 @@
+import React from 'react'
+import {
+    Button,
+    Card,
+    CardActionArea,
+    CardActions,
+    CardContent,
+    Typography,
+} from '@material-ui/core'
+import SafeAgreementIcon from '../SafeAgreement/icon.png'
+import FoundersAgreementIcon from '../FoundersAgreement/icon.jpg'
+import TermsAgreementIcon from '../TermsAgreement/icon.png'
+import './style.css'
+
+const Documents = ({ onSelectDocument }) => {
+    const documents = [
+        {
+            image: SafeAgreementIcon,
+            title: 'Safe agreement',
+            description:
+                'Woooow! Congrats on your first investors! Lets get your SAFE ready in minutes, get some funding and get back to work. This are just the first steps. Round A awaits ahead. Click the button below and star drafting!',
+            buttonTitle: 'Select',
+            buttonDisabled: false,
+            onClick: onSelectDocument,
+        },
+        {
+            image: FoundersAgreementIcon,
+            title: 'Founders agreement',
+            description:
+                "When you just decided to begin your startup journey it's always a good idea to lay out some ground rules with your co-founders. This service can help you with this. Click the button below and start drafting!",
+            buttonTitle: 'Soon',
+            buttonDisabled: true,
+            onClick: () => console.log('Not enabled'),
+        },
+        {
+            image: TermsAgreementIcon,
+            title: 'Terms agreement',
+            description:
+                'If you are starting your SaaS company or building an app, you can count on this Service to create Terms of Service for you. You tailor it to the needs and processes of your business by answering simple questions. Just click a button below and start drafting!',
+            buttonTitle: 'Soon',
+            buttonDisabled: true,
+            onClick: () => console.log('Not enabled'),
+        },
+    ]
+    return (
+        <div className="Documents">
+            {documents.map(doc => (
+                <Card key={doc.title}>
+                    <CardActionArea onClick={doc.onClick}>
+                        <img src={doc.image} alt={doc.title} />
+                        <CardContent>
+                            <Typography gutterBottom variant="h5" component="h2">
+                                {doc.title}
+                            </Typography>
+                            <Typography variant="body2" color="textSecondary" component="p">
+                                {doc.description}
+                            </Typography>
+                        </CardContent>
+                    </CardActionArea>
+                    <CardActions>
+                        <Button
+                            size="small"
+                            color="primary"
+                            onClick={doc.onClick}
+                            disabled={doc.buttonDisabled}
+                        >
+                            {doc.buttonTitle}
+                        </Button>
+                    </CardActions>
+                </Card>
+            ))}
+        </div>
+    )
+}
+
+export default Documents
--- src/Components/Layout/index.jsx
+++ src/Components/Layout/index.jsx
@@ -1,12 +1,44 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './style.css'
-import { Container } from '@material-ui/core'
+import { Container, AppBar, Typography, Toolbar } from '@material-ui/core'
+import { makeStyles } from '@material-ui/core/styles'
+import SafeAgreement from '../SafeAgreement'
+import Documents from '../Documents'
+
+const useStyles = makeStyles({
+    card: {},
+    media: {
+        height: 140,
+    },
+})
 
 const Layout = ({}) => {
+    const [questions, setQuestions] = useState({})
+    const [isShowFoundersAgreement, setShowFoundersAgreement] = useState(false)
     return (
-        <Container>
-            <div>Hello world</div>
-        </Container>
+        <>
+            <AppBar position="static" className="Layout__appbar">
+                <Toolbar>
+                    <Typography variant="h6">DocuSign Hackhaton</Typography>
+                </Toolbar>
+            </AppBar>
+            <Container>
+                {!isShowFoundersAgreement && (
+                    <>
+                        <Typography variant="h2" color="primary" align="center" gutterBottom>
+                            Free legal documents for startups
+                        </Typography>
+                        <Typography variant="h4" align="center" gutterBottom>
+                            Choose a document to draft
+                        </Typography>
+
+                        <Documents onSelectDocument={() => setShowFoundersAgreement(true)} />
+                    </>
+                )}
+
+                {isShowFoundersAgreement && <SafeAgreement onAnswerSelect={setQuestions} />}
+            </Container>
+        </>
     )
 }
 
